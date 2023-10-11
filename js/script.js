732df50283a2e09230fb09ed4fc957c1ccd24314
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-11 16:42:26 +0200

    Added

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const wrapperEl = document.getElementById('boxWrapper');
 
 //Ciclo
 
-for(let i = 1; i <= 7; i++){
-    for(let c = 1; c <= 7; c++){
+for (let i = 1; i <= 7; i++) {
+    for (let c = 1; c <= 7; c++) {
         console.log(i);
         console.log(c);
         let bgBox;
         //Condizioni
-        if(i % 3 > 0){
-            bgBox ='even';
-        } else{
-            bgBox ='odd';
-        } if (c % 3 === 0){
+        if (i % 3 > 0) {
+            bgBox = 'even';
+        } else {
             bgBox = 'odd';
-        } else{
+        } if (c % 3 === 0) {
+            bgBox = 'odd';
+        } else {
             bgBox = 'even';
         }
 
@@ -22,30 +22,29 @@ for(let i = 1; i <= 7; i++){
         boxEl.className = ` box ${bgBox} d-flex justify-content-center align-items-center`;
         boxEl.innerHTML = c * i;
         wrapperEl.append(boxEl);
-
     }
 }
 
-for(let d = 1; d <= 5; d++){
-    for(let e = 1; e <= 5; e++){
+for (let d = 1; d <= 7; d++) {
+    for (let e = 1; e <= 7; e++) {
         console.log(d);
         console.log(e);
         let bgBox;
         //Condizioni
-        if(d % 5 > 0){
-            bgBox ='even2';
-        } else{
-            bgBox ='odd2';
-        }if(e % 5 === 0){
+        if (d % 5 > 0) {
+            bgBox = 'even2';
+        } else {
             bgBox = 'odd2';
-        } else{
-            bgBox ='even2';
+        } if (e % 5 === 0) {
+            bgBox = 'odd2';
+        } else {
+            bgBox = 'even2';
         }
 
         const boxEl = document.createElement('div');
         boxEl.className = ` box ${bgBox} d-flex justify-content-center align-items-center`;
         boxEl.innerHTML = d * e;
         wrapperEl.append(boxEl);
-
     }
 }
+
