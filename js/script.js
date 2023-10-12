92c985655f1b5646323506b20559b24d50cf68ad
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-12 11:04:05 +0200

    Corrections

--- js/script.js
+++ js/script.js
@@ -2,42 +2,35 @@ const wrapperEl = document.getElementById('boxWrapper');
 
 //Ciclo
 
-for (let i = 1; i <= 10; i++) {
-    for (let c = 1; c <= 10; c++) {
-        console.log(i);
-        console.log(c);
-        let bgBox;
+for (let i = 1; i < 101; i++) {
+
+        let bgBox, text;
         //Condizioni
         // se il numero è divisibile per 3 e 5
         if (i % 3 === 0 && i % 5 === 0) {
-            bgBox = 'even';
+            bgBox = 'odd2';
+            text = `FizzBuzz`;
         }
         // se è divisibile per 3
         else if (i % 3 === 0) {
             bgBox = 'odd';
+            text = `Fizz`;
         }
         // se è divisibile per 5
         else if (i % 5 === 0) {
-            bgBox = 'odd';
+            bgBox = 'even2';
+            text =  `Buzz`;
          }
         // altrimenti
         else {
             bgBox = 'even';
+            text = (i);
          }
         const boxEl = document.createElement('div');
         boxEl.className = ` box ${bgBox} d-flex justify-content-center align-items-center`;
-        boxEl.innerHTML = c * i;
+        boxEl.innerText = text;
         wrapperEl.append(boxEl);
-    }
+
 }
 
 
-/*if (i % 3 > 0) {
-    bgBox = 'even';
-} else {
-    bgBox = 'odd';
-} if (c % 3 === 0) {
-    bgBox = 'odd';
-} else {
-    bgBox = 'even';
-}*/
